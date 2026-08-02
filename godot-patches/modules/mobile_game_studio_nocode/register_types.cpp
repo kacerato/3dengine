a8a612a8f1b9def4533df389dd60eac7443d9d93
kacerato <79872194+kacerato@users.noreply.github.com>
2026-08-02 10:50:45 -0300

    feat(nocode): register graph runtime and editor plugin

---
+++ godot-patches/modules/mobile_game_studio_nocode/register_types.cpp
@@ -0,0 +1,25 @@
+#include "register_types.h"
+
+#include "mgs_nocode_graph.h"
+#include "mgs_nocode_runner.h"
+#include "core/object/class_db.h"
+
+#ifdef TOOLS_ENABLED
+#include "editor/mgs_nocode_editor_plugin.h"
+#include "editor/plugins/editor_plugin.h"
+#endif
+
+void initialize_mobile_game_studio_nocode_module(ModuleInitializationLevel p_level) {
+    if (p_level == MODULE_INITIALIZATION_LEVEL_SCENE) {
+        GDREGISTER_CLASS(MGSNoCodeGraph);
+        GDREGISTER_CLASS(MGSNoCodeRunner);
+    }
+#ifdef TOOLS_ENABLED
+    if (p_level == MODULE_INITIALIZATION_LEVEL_EDITOR) {
+        EditorPlugins::add_by_type<MGSNoCodeEditorPlugin>();
+    }
+#endif
+}
+
+void uninitialize_mobile_game_studio_nocode_module(ModuleInitializationLevel p_level) {
+}
